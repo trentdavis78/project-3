4bd5bb430e4627382dd960d970f6dc74b4744041
Trent Davis <trentdavisinc@gmail.com>
2019-05-14 23:49:25 -0500

    React App ready

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-div">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
